c374c6efc1b289f3fcc3f656e9e2538a6a50b6b5
André Guimarães <andrepaivaguimaraes@gmail.com>
2023-10-29 19:43:49 -0300

    publicando nosso jogo

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,27 @@
+const elementoChute = document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    verificaSeChutePossuiValorValido(chute)
+}
+
+function exibeChuteNaTela(chute) {
+    console.log(chute)
+    elementoChute.innerHTML = `
+    <div>Você disse</div>
+    <span class="box">${chute}</span>
+    `
+}
+
+recognition.addEventListener("end", () => {    
+    recognition.start();
+});
